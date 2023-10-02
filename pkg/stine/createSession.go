db6fa6a62bf830156d967a9aa622e5baeea18fe6
Marten <derapfelesser@aol.de>
2023-10-02 13:27:03 +0200

    feat(createSession): include authToken in form request

--- pkg/stine/createSession.go
+++ pkg/stine/createSession.go
@@ -39,12 +39,13 @@ func makeAntiforgeryCookieAndGetAuthToken(client *http.Client) (string, error) {
 	return authToken, nil
 }
 
-func makeIDSRVCookies(client *http.Client, username string, password string) error {
+func makeIDSRVCookies(client *http.Client, username string, password string, authToken string) error {
 	reqURL := "https://cndsf.ad.uni-hamburg.de/IdentityServer/Account/Login?ReturnUrl=%2FIdentityServer%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3DClassicWeb%26scope%3Dopenid%2520DSF%26response_mode%3Dquery%26response_type%3Dcode%26nonce%3DkQEKs7lCwN2CEXvCDeD1Zw%253D%253D%26redirect_uri%3Dhttps%253A%252F%252Fstine.uni-hamburg.de%252Fscripts%252Fmgrqispi.dll%253FAPPNAME%253DCampusNet%2526PRGNAME%253DLOGINCHECK%2526ARGUMENTS%253D-N000000000000001,ids_mode%2526ids_mode%253DY"
 	formData := url.Values{
-		"Username":      {username},
-		"Password":      {password},
-		"RememberLogin": {"true"},
+		"Username":                 {username},
+		"Password":                 {password},
+		"RequestVerificationToken": {authToken},
+		"RememberLogin":            {"true"},
 	}
 	resp, err := client.PostForm(reqURL, formData)
 	if err != nil {
@@ -66,7 +67,7 @@ func GetSession(username string, password string) error {
 	if antiForgeryAuthError != nil {
 		return antiForgeryAuthError
 	}
-	idsrvError := makeIDSRVCookies(client, username, password)
+	idsrvError := makeIDSRVCookies(client, username, password, authToken)
 	if idsrvError != nil {
 		return idsrvError
 	}
