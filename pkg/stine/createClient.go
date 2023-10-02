8b9b0adadc54eac0a920697d0b70668e03856735
Marten <derapfelesser@aol.de>
2023-10-02 14:46:13 +0200

    feat(createClient): include userAgent in each request

--- pkg/stine/createClient.go
+++ pkg/stine/createClient.go
@@ -7,6 +7,13 @@ import (
 	"net/http/cookiejar"
 )
 
+type myTransport struct{}
+
+func (t *myTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+	return http.DefaultTransport.RoundTrip(req)
+}
+
 func getCookieJar() http.CookieJar {
 	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	if err != nil {
@@ -17,6 +24,7 @@ func getCookieJar() http.CookieJar {
 
 func getClient() *http.Client {
 	return &http.Client{
-		Jar: getCookieJar(),
+		Jar:       getCookieJar(),
+		Transport: &myTransport{},
 	}
 }
