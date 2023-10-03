41f918dca0ee8348d7b2bd1cb1c6b6ab52226228
Marten <derapfelesser@aol.de>
2023-10-03 19:09:17 +0200

    feat(createClient): do not use cookieJar

--- pkg/stine/createClient.go
+++ pkg/stine/createClient.go
@@ -1,10 +1,7 @@
 package stine
 
 import (
-	"golang.org/x/net/publicsuffix"
-	"log"
 	"net/http"
-	"net/http/cookiejar"
 )
 
 type myTransport struct{}
@@ -14,17 +11,8 @@ func (t *myTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return http.DefaultTransport.RoundTrip(req)
 }
 
-func getCookieJar() http.CookieJar {
-	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
-	if err != nil {
-		log.Fatal(err)
-	}
-	return jar
-}
-
 func getClient() *http.Client {
 	return &http.Client{
-		Jar:       getCookieJar(),
 		Transport: &myTransport{},
 	}
 }
