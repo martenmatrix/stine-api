c175dc9731dd699efcb77d9b19ceba21df5bc94a
Marten <derapfelesser@aol.de>
2023-10-08 16:43:06 +0200

    test(createSession): test getSessionNo

--- createSession_test.go
+++ createSession_test.go
@@ -105,3 +105,12 @@ func TestGetMalformattedCNSCCookie(t *testing.T) {
 		t.Errorf("Cookies name differs from response name")
 	}
 }
+
+func TestGetSessionNo(t *testing.T) {
+	sessionURL := "https://stine.uni-hamburg.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=MLSSTART&ARGUMENTS=-N899462345432351,-N000266,"
+	sessionNo := getSessionNo(sessionURL)
+
+	if sessionNo != "899462345432351" {
+		t.Errorf("WANT: 899462345432351, GOT: %s", sessionNo)
+	}
+}
