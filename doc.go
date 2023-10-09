e1f58b1e8b88db44badd7fe10874210403bf773e
Marten <derapfelesser@aol.de>
2023-10-09 22:50:26 +0200

    feat(doc): add docfile

---
+++ doc.go
@@ -0,0 +1,17 @@
+/*
+This is an unofficial STiNE API for Go. It is easy to use, completely request-based and uses no browser automation, which makes it incredible fast.
+
+Basic Usage:
+
+	session := stineapi.NewSession()
+
+	err := session.Login("user", "pass")
+	if err != nil {
+	// handle error
+	}
+
+	// our client is authenticated on the stine website, session provides various functions to interact with it
+
+*/
+
+package stineapi
