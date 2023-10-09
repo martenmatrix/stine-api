bead60cf3735a4e451f18a72dfd9411c4b66316c
Marten <derapfelesser@aol.de>
2023-10-09 23:26:36 +0200

    feat(getUserData): create type for general info from user account

---
+++ getUserData.go
@@ -0,0 +1,12 @@
+package stineapi
+
+type general struct {
+	MatriculationNumber string
+	Name                string
+	ForwardToUniEmail   bool
+	SecondCitizenship   string
+	Phone               string
+	Mobile              string
+	Mail                string
+	UniMail             string
+}
