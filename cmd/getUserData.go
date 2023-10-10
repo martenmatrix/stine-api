c98bd84a1578eaaed7c197c35616cbce4ef5aac7
Marten <derapfelesser@aol.de>
2023-10-10 15:24:08 +0200

    test(getUserData): implement getUserAccountURL and add function body for getUserAccount

--- cmd/getUserData.go
+++ cmd/getUserData.go
@@ -1,5 +1,7 @@
 package stineapi
 
+import "fmt"
+
 type general struct {
 	MatriculationNumber string
 	Name                string
@@ -28,4 +30,12 @@ type UserData struct {
 	General    general
 	Address    address
 	Statistics statistics
+}
+
+func (session *Session) getUserAccountURL() string {
+	return fmt.Sprintf("https://stine.uni-hamburg.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=PERSADDRESS&ARGUMENTS=-N%s,-N000273,", session.sessionNo)
+}
+
+func (session *Session) GetUserData() (UserData, error) {
+	return UserData{}, nil
 }
