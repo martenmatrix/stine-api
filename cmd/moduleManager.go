6cac73eb7487ddfe9a90caacde34e9d296695953
Marten <derapfelesser@aol.de>
2023-10-12 15:31:09 +0200

    feat(moduleManager): add function to initalize module reg

--- cmd/moduleManager.go
+++ cmd/moduleManager.go
@@ -1,5 +1,22 @@
 package main
+package stineapi
+
+import (
+	"errors"
+	"github.com/PuerkitoBio/goquery"
+	"regexp"
+)
+
+type ModuleRegistration struct {
+	TanRequired   bool
+	TanStartsWith string
+	SetTan        func(tan string)
+}
 
 func main() {
 
+func (session *Session) CreateModuleRegistration(registrationLink string) ModuleRegistration {
+	url := session.replaceSessionNumber(registrationLink)
+	session.getRegistrationId(url)
+	return ModuleRegistration{}
 }
