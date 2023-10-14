f675ad7c8f6b74dea56ef1258ae0f7ba8d8f8ffc
Marten <derapfelesser@aol.de>
2023-10-14 12:49:14 +0200

    test(moduleManager): reference from session is being used in moduleRegistration

--- cmd/moduleManager_test.go
+++ cmd/moduleManager_test.go
@@ -1,18 +1,8 @@
 package stineapi
 
 import (
-	"log"
 	"testing"
 )
-
-func TestArgsWithoutSessionNumber(t *testing.T) {
-	ses := NewSession()
-	err := ses.Login("BBB8542", "Xubnyg-pohpek-fytce2")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ses.CreateModuleRegistration("https://stine.uni-hamburg.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=REGCOURSEMOD&ARGUMENTS=-N266412591351119,-N000309,-N383764423011169,-AMOFF,-N386935112285453,-N384245998551730,-N0,-N0,-N0,-AN,-N0")
-}
 
 func TestCreateModulRegistration(t *testing.T) {
 	fakeRegistrationLink := "https://stine.uni-hamburg.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=REGCOURSEMOD&ARGUMENTS=-N232343443351119,-N343449,-N343424234011169,-ADOFF,-N343434342285453,-N344343434341730,-N0,-N0,-N0,-AN,-N0"
@@ -27,5 +17,13 @@ func TestCreateModulRegistration(t *testing.T) {
 }
 
 func TestReferenceCopyOfSessionIsUsed(t *testing.T) {
+	ses := NewSession()
+	moduleReg := ses.CreateModuleRegistration("https://www.example.org")
+
+	ses.sessionNo = "changed"
+
+	if moduleReg.session.sessionNo != ses.sessionNo {
+		t.Error("no reference of session is used")
+	}
 
 }
