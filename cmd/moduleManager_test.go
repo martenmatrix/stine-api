314d0b093a3dd93b3321f68aa39ff0dcb8bf9e07
Marten <derapfelesser@aol.de>
2023-10-14 13:55:47 +0200

    test(moduleManager): registration id is parsed from response and set on struct

--- cmd/moduleManager_test.go
+++ cmd/moduleManager_test.go
@@ -1,6 +1,9 @@
 package stineapi
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -39,5 +42,25 @@ func TestReplaceSessionNumber(t *testing.T) {
 
 	if moduleReg.registrationLink != "https://stine.uni-hamburg.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=REGCOURSEMOD&ARGUMENTS=-N"+newSessionNo+",-N343449,-N343424234011169,-ADOFF,-N343434342285453,-N344343434341730,-N0,-N0,-N0,-AN,-N0" {
 		t.Error("session number is not being replaced in link")
+	}
+}
+
+func TestGetRegistrationId(t *testing.T) {
+	fakeRegistrationId := "2132134"
+	fakeServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<input name="rgtr_id" value="` + fakeRegistrationId + `"/>`))
+	}),
+	)
+	ses := NewSession()
+	modReg := ses.CreateModuleRegistration("https://www.example.org")
+	modReg.registrationLink = fakeServer.URL
+
+	err := modReg.getRegistrationId()
+
+	if err != nil {
+		t.Error(err)
+	}
+	if modReg.registrationId != fakeRegistrationId {
+		t.Error(fmt.Sprintf("EXPECTED: %s, RECEIVED: %s", fakeRegistrationId, modReg.registrationId))
 	}
 }
