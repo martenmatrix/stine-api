c3a700f6d7db10b68312653e4089142776096472
Marten <derapfelesser@aol.de>
2024-03-11 16:42:32 +0100

    doc(package comment): display overview comment in doc

--- cmd/doc.go
+++ cmd/doc.go
@@ -1,17 +1,4 @@
 /*
 Package stineapi is an unofficial STiNE API for Go. It is easy to use, completely request-based and uses no browser automation, which makes it incredible fast.
-
-Basic Usage:
-
-	session := stineapi.NewSession()
-
-	err := session.Login("user", "pass")
-	if err != nil {
-	// handle error
-	}
-
-	// our client is authenticated on the stine website, session provides various functions to interact with it
-
 */
-
 package stineapi
